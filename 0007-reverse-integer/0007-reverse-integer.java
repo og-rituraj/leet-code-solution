a91638f9a5035c3127c0e20cf579598ad376c193
Rituraj <rituraj52003@gmail.com>
2026-09-10 13:41:39 +0530

    Time: 1 ms (99.97%), Space: 42.9 MB (11.48%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -5,8 +5,8 @@ class Solution {
 
         while (x != 0) {
 
+            // Get the last digit
             int digit = x % 10;
-            x = x / 10;
 
             // Check overflow before multiplying by 10
             if (result > Integer.MAX_VALUE / 10 ||
@@ -19,7 +19,11 @@ class Solution {
                 return 0;
             }
 
+            // Add digit to result
             result = result * 10 + digit;
+
+            // Remove last digit from x
+            x = x / 10;
         }
 
         return result;
